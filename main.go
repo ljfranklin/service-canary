41a9f5696fb365cf6a3a1a83b1f29c34bbd9d0dd
Lyle Franklin <lfranklin@pivotal.io>
2015-12-18 12:34:41 -0800

    Create top-level objects in main

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ljfranklin/service-canary/config"
+	"github.com/ljfranklin/service-canary/scheduler"
+	"github.com/ljfranklin/service-canary/service-factory"
+	"github.com/ljfranklin/service-canary/service-manager"
+)
 
 func main() {
-	fmt.Println("Hello...")
+	rootConfig := config.New()
+	err := rootConfig.Validate()
+	if err != nil {
+		rootConfig.Logger.Fatal("Failed to validate config", err)
+	}
+
+	factory := service_factory.New(rootConfig)
+
+	manager := service_manager.New(factory, rootConfig)
+
+	scheduler := scheduler.New(manager, rootConfig)
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- scheduler.RunInBackground()
+	}()
+
+	err = <-errChan
+	rootConfig.Logger.Fatal("App exited unexpectedly", err)
 }
